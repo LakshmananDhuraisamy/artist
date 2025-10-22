9c83ac0d85f745abba7b1eca07a59cbb6fce4e22
Lakshmanan D <dlakshmananbe@gmail.com>
2025-10-22 20:29:53 +0530

    .htaccess with new php files

---
+++ about.php
@@ -0,0 +1,53 @@
+<?php include('header.php');?>
+
+<body>
+
+    <nav>
+        <div class="logo">
+            <a href="index.html"><em>Williams</em></a>
+        </div>
+      <div class="menu-icon">
+        <span></span>
+      </div>
+    </nav>
+
+    <div class="page-heading">
+        <div class="container">
+            <div class="heading-content">
+                <h1>About <em>Me</em></h1>
+            </div>
+        </div>
+    </div>
+ 
+    <div class="more-about-us">
+        <div class="container">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="content">
+                    <h2>About Me</h2>
+                    <span>William</span>
+                    <p>
+                        Hi, my name's William, I'm 36 years old (or at least I was at the time of writing this).
+I was born in New Zealand but grew up in Australia, in a small town in suburban Queensland. I like to paint and write.
+I spent most of my 20s, on and off, trying to write, edit, and rewrite a few novels of mine...<br><br>
+about various experiences I'd had that stuck with me... If you're interested, you can find out more about them and download them for free under the writing tab on this site...
+If you read and appreciated one, please consider leaving me a five or ten dollar tip—it really helps me out. You're also welcome to get in touch by email and let me know what you thought of the work; my email is also listed here on the site.
+Regarding the writing, Kerouac, Dahl, Hemingway, Orwell, and Shakespeare were always big influences for me. 
+<br><br>
+As well as the writing, I also tried my hand at painting, starting in earnest pretty much from around when I was 30, during the Covid-19 lockdowns when I had a lot of time on my hands... and you can see some of my pictures here too on the site...
+Monet, Gauguin, and Van Gogh were big influences for me there, amongst others... I won't make a long list...
+<br><br>
+I'm also avaliable for art lessons, either in person if we're in the same locale, or via web call, and I can teach you how to paint in my style or even how to find your own... and you can find out more on the teaching page on my site.
+<br><br>
+Anyway... thanks for stopping by...<br>
+Hope you have a nice day, <br>
+Yours sincerely, <br>
+William <br>
+</p>
+                    <div class="simple-btn">
+                        <a href="#">Continue Reading</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ <?php include('footer.php');?>
