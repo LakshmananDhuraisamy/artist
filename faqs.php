9d592c75c22dbbceb6b9bbbe897478335d2007fc
Lakshmanan D <dlakshmananbe@gmail.com>
2025-10-23 14:38:36 +0530

    faq other contents

--- faqs.php
+++ faqs.php
@@ -120,8 +120,6 @@ And the way we treat them in this day and age, is by any reasonable moral standa
     </div>
     </div>
 
-
-
   
 </div>
 
@@ -129,4 +127,61 @@ And the way we treat them in this day and age, is by any reasonable moral standa
             </div>
         </div>
     </div>
+
+     <div class="more-about-us" style="margin-top: 0px;">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="content" style="    padding: 5px 40px;">
+                  
+                    <p>
+                     
+                    
+Globally, around 70–80 billion chickens are raised and slaughtered for meat every single year. That’s not counting the ones who die inside the gigantic sheds before slaughter, as they're too weak to get up and access the waterers or feeders, or the hundreds of millions of male chicks from the egg industry who are killed right after hatching because they don’t lay eggs. In the U.S. alone it’s about 9 billion a year, in the EU another 6 billion, and in Asia its even higher.
+<br>
+If you zoom out, that’s more sentient creatures than there are humans alive on the planet, all going through this channel of birth, confinement, slaughter every twelve months. 
+<br>
+And a lot of the Amazon, which is in a precarious state as is, has to be razed to grow cattle feed on to feed them all. 
+<br>
+So there it is, if you accept that animals are just like us, feeling things and not wanting to die, and they don't want to be "humanely slaughtered" (read gassed to death, electrocuted, shot in the head or having their throats slit open) any more than you would, and that as far as we have evidence for it, when you die, that's the end of your conscious experience here forever, then what we're doing to these creatures and the negative effects it's having on us and our planet, in terms of deforestation, poisoned seas, zootropic cross-over pandemic risks, acceleration of the mass extinction event we're living thru and just the mass suffering and dread of beings no less worthy nor greatly different to ourselves, is a crime, we should feel a deep penitence for and immediately remedy. 
+<br>
+But you try screaming that at such a powerful machine when you're watching all of these vegan slaughterhouse "farm" exposes, you do feel a little powerless.
+And yet, at the same time I do believe there is goodness and kindness and love too in humanity and it just needs to be remembered and returned to and nurtured. That instinct when you see someone--anyone-- suffering in whatever body they're in and say 'fuck's sake, can't someone help them? Are they ok?' That's hard wired into us and our DNA and that's why the industry is so intent on not letting the truth of the neglect and cruelty these systems are built upon getting out.
+<br>
+And, yes you can, check up on a few animal rights exposes on YouTube, Joey Carbstrong and Earthling Ed, I can recommend, and look at some of these weak little birds dying on the floors of these sheds, and know where some of these products are coming from and then be in the supermarket and not reach for the 'chicken breast fillets' or the 'mother and calf separated, short life of slavery, beatings and misery milk' but the soya milk say instead, or the 'harried into the crammed iron gondola that rattles down into the gas, pork sausages' but their vegan alternative. 
+<br>
+Change is possible. <br>
+Healing is possible. <br>
+Redemption is possible. <br>
+Peace, clarity, love, kindness and understanding are all definitely still possible. For everyone. It's not too late for anyone of us here, I sincerely believe.
+<br><br>
+And deep down on some level I think we all know this is wrong, chickens aren’t meant to live a fraction of their natural lifespans crammed into cages and sheds with dead and dying birds, acrid ammonium and excrement everywhere.
+They’re jungle fowl at heart, descended from birds in the forests of Southeast Asia. Scratching for insects, roosting in trees, dust-bathing, calling to each other in twilight  — that’s the natural script written into them. 
+And instead, we rewrote it: “grow so fast your legs snap, live in a windowless shed, die young"
+<br><br>
+The whole system runs on hiding.<br>
+Hide the shed behind steel walls. Hide the suffering behind “farm fresh” labels. Hide the bodies behind nuggets and burgers so small and uniform you can’t even tell they were once birds. It’s not just cruel — it’s dishonest.
+<br><br>
+But, change is afoot people once thought slavery was natural, people once thought burning witches was godly-- now, most of us look back at those eras with revulsion. There’s a chance — a real one — that animal exploitation will one day be seen with the same shame and regret. 
+<br><br>
+So let's be on the right side of history... let's do all we can to respect ourselves and each other here. After all, we're in this struggle of life together here right? 
+And if everyone could be happy and free and all the nature could be grown back, how beautiful this life and planet could still yet be
+I kindly invite you to check out my animal welfare petition here.
+https://www.change.org/p/animal-lives-matter-0057766d-cdbd-4901-93d1-5fb001c15a1c
+<br><br>
+And if you got all of the way to the bottom of this thanks for reading, it's a grim read but I felt it needs to be said.
+<br>
+
+
+William Seymour Jones<br>
+Trouble.underwater@gmail.com
+</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
  <?php include('footer.php');?>
