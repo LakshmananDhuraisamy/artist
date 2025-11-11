e15ad332453ef56dae20197edb67b85d853e53a4
Lakshmanan D <dlakshmananbe@gmail.com>
2025-11-11 15:14:10 +0530

    writing novels section

--- writings.php
+++ writings.php
@@ -2,23 +2,23 @@
 
     <div class="more-about-us">
         <div class="container">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12">
                 <div class="content">
                     <h2>Writings</h2>
                     <span>Poems | Novels </span>
                     <p>
+ <h1 style="    color: #f4dd5b;">  Poems </h1> <br>
 
 <div class="image  autour-bio-image" >
-                    <img style="width: 100%;height: 350px;" src="img/poem-1.jpeg" alt="">
+                    <img style="width: 100%;height: 450px;" src="img/poem-1.jpeg" alt="">
                 </div>
-<b>Poems</b> <br>
 A Eulogy for Animals <br>
 Man to beast is the looming figure of danger, <br>
 a violater, a killer, all our human virtues and artistry bereft, and merely the hand that grabs, the boot that kicks, the figure that comes for their young, the wielder of  the chain, the blade, the rod, the gun, the milking and maiming apartus, towering figures devoid of mercy, threats to be avoided and likely reviled... And yet, still from behind the bars, the wet forgiving nose, the curious snout comes to nuzzle the hand that rips child from mother. The scope unfathomable, our savagery, immediate and viscerally met, and for all our smiling emojis and words to soothe, a sundered ship, that if driven on will surely founder, we will not fully understand these, the weight of  our actions, until the oblivion we've inflicted on so many  rushes to meet us in the most unwelcome sense. <br>
 
 <br>
 <div class="image  autour-bio-image" >
-                    <img style="width: 100%;height: 450px;" src="img/poem-2.jpeg" alt="">
+                    <img style="width: 100%;height: 550px;" src="img/poem-2.jpeg" alt="">
                 </div><br>
 Inside the gondola <br>
 Like a candle in a storm we see you,<br>
@@ -26,8 +26,6 @@ dear friend, in the shed, the cage, the lowering acrid gondola. Dear friend we s
 <br>
 
 <br>
-
-<b>Novels </b> <br>
 </p>
 <style>
 .download-btn {
@@ -54,60 +52,100 @@ dear friend, in the shed, the cage, the lowering acrid gondola. Dear friend we s
 
 
 </style>
-        <div class="container-fluid" style="margin-bottom: 20px;">
-        <?php
-        $doc_array = array("Abby", "Claire", "Doggo", "Maria", "Patches", "Short_Stories");
-        
-        foreach ($doc_array as $doc) {
-            ?> 
-            <a target="_blank" href="img/docs/<?php echo $doc; ?>.pdf" class="download-btn btn"><i class="fa fa-download"></i>  <?php echo $doc; ?></a>
-        <?php } ?>
-        </div>
- <br><br><br>
-<div class="full-screen-portfolio" id="portfolio">
-        <div class="container-fluid">
-            <div class="col-md-4 col-sm-6">
-                <div class="portfolio-item">
-                    <a href="img/big_portfolio_item_4.png" data-lightbox="image-1"><div class="thumb">
-                        <div class="hover-effect">
-                            
-                        </div>
-                        <div class="image">
-                            <img src="img/portfolio_item_4.png">
-                        </div>
-                    </div></a>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <div class="portfolio-item">
-                    <a href="img/big_portfolio_item_2.png" data-lightbox="image-1"><div class="thumb">
-                        <div class="hover-effect">
-                            
-                        </div>
-                        <div class="image">
-                            <img src="img/portfolio_item_2.png">
-                        </div>
-                    </div></a>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <div class="portfolio-item">
-                    <a href="img/big_portfolio_item_2.png" data-lightbox="image-1"><div class="thumb">
-                        <div class="hover-effect">
-                            
-                        </div>
-                        <div class="image">
-                            <img src="img/portfolio_item_2.png">
-                        </div>
-                    </div></a>
-                </div>
-            </div>
+ 
+<div class="container-fluid" style="margin-bottom: 20px;">
+ <h1 style="    color: #f4dd5b;">  Novels </h1> 
+ 
+        <p> Whilst I naturally drew from life a little here and there in writing these stories I fictionalised a lot of them and people change and everyone, myself perhaps most of all, make mistakes in this life and deserve second chances and the benefit of the doubt, and none of the characters are meant to be negative or defamatory depictions of anyone I know or have known personally... 
+And my desire wasn't to hurt, upset or defame anyone in writing these stories. 
+</p><br>
 
-            
+        <?php
+        $doc_title_array = array("Maria", "Claire", "Undiscovered Country", "Patches", "Doggo", "Max WRLD", "Lucy", "Abby", "The Good Witch", "Short Stories");
+         $doc_file_array =   array("Maria.pdf", "Claire.pdf", "Undiscovered_Country.docx", "Patches.pdf", "Doggo.pdf", "Max_Wrld.docx", "Lucy.docx", "Abby.pdf", "The_Good_Witch.docx", "Short_Stories.pdf");
+         $doc_content_array=  array("Maria tells the story, of Max, an unsuccessful amateur writer and lonely young man, who, with his latest writing project finished and not much going on in his life, finds himself at a bit of a loss... that is until he meets an eccentric middle aged fellow by the name of James in a quiet lakeside cafe in Pokhara, Nepal, one afternoon. The pair politely greet each other and soon get to talking and as James tells Max a bit about himself and his story, he claims that he was a close friend of the late prolific painter and artist Pablo Picasso, having apparently had lived with him in a commune in Paris for a spell and even having been a lover of his in a homosexual manage-trois with another fellow for a spell.
+            <br> 
+James soon heads on for the Nepali capital, Kathmandu, on some business or another, and feeling quite listless, directionless and bored with his life at the time, Max, soon decides to rent a motorscooter and set out for the capital to meet this queer James fellow, and see if he can't interview him and write his story, see if he really was a lover and intimate of Picasso or rather just some bored eccentric pulling his leg, either way, Max figures it'll give him a project to work towards and something to do, which is better than nothing and just loping around and feeling sorry for himself all day. 
+<br><br>
+One afternoon in a café, over some coffees, which my contact added some whiskey to from a small metal flask of his, he said to me, ‘I was thinking, perhaps, my boy, that you might like to build for us a farady cage’
+‘A farady cage?’ I asked.
+<br><br>
+‘Essentially you would just need to affix a few layers of aluminum foil and saran wrap to all the surfaces—floors, walls, ceilings of your room, say uh, with a staple gun and we could go from there’
+‘Oh?’<br><br>
+‘And if you were to do that, for us, then inside the NSA would no longer be able to track any of my thoughts; you see, during my brief tenure with them at Fort Meade, they had me take an EEG mapping of my brain…’
+‘Oh yes’<br><br>
+‘And so, yes, if you were able to arrange that for me, my boy, then, I’d be at liberty to tell you, well, just about everything; the real, juicy shit about, well, about just how far deep this rabbit hole really goes…’",
+
+
+"Claire was written about an old friend I first met in a youth hostel in Berlin in my early twenties where I was editing my first novel Lucy again there at the time. <br><br>
+And this was back when I had very little to do with women and not much in the way of self-esteem or self-confidence either, so I was naturally quite pleased and excited to have this new female friend. I'd first seen her in the common room of the hostel dressed in dark gothic clothes, perhaps looking a little like a young Madonna, in her black dress and tights, her outfit complete with a little crucifix that she wore around her neck. And when I first got to talking to her in the common room that afternoon I asked her if she was a Christian and if that was why she wore the crucifix for and she said no and that she just liked camp things and I told her that I felt certain that we were going to be good friends, which she laughed at. Claire stayed on in the hostel and the cry for about a week or so and we walked around the streets of Berlin getting drunk and looking for party drugs together to no avail, tho she did take me to an abandon Iraqi embassy for some urban exploring one afternoon, which was quite fun, with the trashed old building and all of the Arabic files on the floor. It never got romantic between the two of us in Berlin tho. And both being from Australia, when I eventually got back to Aus I went down to Sydney for a bit to visit her and this story or novella is about that...
+<br><br>
+Claire and I then walked back to the bottle shop we’d been to before and bought another bottle of Mixed Berry. We handed it back and forth drinking it as we walked on thru whichever suburb we were in. After we’d finished drinking it in some side street or another I said to Claire, ‘hey do you want to see something cool?’
+‘Yes’ she replied and I then got the bottle and threw it as high as I could up in the air. It soon smashed down onto the dark road before us.
+‘Hey!’ someone shouted out from one of the houses on the side of the street, as they turned a veranda light on. <br><br>
+‘Run! Run! Fucking run!’ I said to Claire and we both ran off as fast as we could.",
+
+"This novel tells the story of the loneliness and isolation, of Max, a young man flown out from his native suburban Australia to NYC, on a 6 month tourist visa, to meet and stay with an old European-American travelling buddy of his, Liam, who he met on a recent trip to Europe. <br><br>
+The two young men share an apartment together in Brooklyn, but Liam is seeing a girl then and seems to have less and less time for his old buddy Max, who comes to spend most of his time in the city, either walking around town at night getting drunk on his own, or locked up in his railroad room staring into his laptop screen, wondering what the hell he is doing. <br><br>
+Adding to his troubles, Max too soon comes to quarrel with Liam and his new girlfriend Marsha, in the cramped little apartment...
+<br><br>
+To me this story is about the outsider, the loner, the one who doesn't quite fit in...<br><br>
+Like a good painting, it's not there to preach any sort of moral nor lesson-- and after all what can one really preach in this world besides that kindness and peace and appreciation are generally good things and cruelty and the like are bad-- but more so to create a snapshot of a lonely and bitter time in my life... not necessarily while I was in New York, where I actually was treated quite charitably a lot of the time and made some good memories and probably was as much of an asshole myself as anyone else (if not more so)... but more so just times in general when I've found myself in bitter or judgmental or uncharitable moods. 
+<br><br>
+'…Listen, you really think death is gonna be better than life?' the fellow said to me, 'At least in life there’s hope, however small. Memories can be bad, but what matters is the present and future. At least that you can do something about. Work for it and maybe your life can get better…’
+‘Yeah, well, thanks man, nice talk' I replied, '…What was your name by the way man?’
+<br><br>
+‘Aaron’<br>
+‘You shake hands?’ I then asked Aaron offering him my hand.<br>
+‘Yeah’ he replied, shaking my hand.<br>
+‘Max’ I told him.",
+
+
+"This one tells the story of when I took in a stray puppy in Jog Falls, India when I was in my early twenties, fairly lonely, fretting about some recently discovered expression lines on my cheeks and editing a novel of mine.",
+
+
+"This one tells the story of my friendship with  a stray dog that I befriended while I was staying on an island in Thailand and editing a novel of mine again there and in something of a lonely and isolated place in my life. ",
+
+
+"The story of my dear ex-wife Kalpana and our honeymoon to Rishikesh, and the harassment I suffered while trying to paint landscapes outside in Nepal and India",
+
+
+"This novel tells the story of Lucy, who's around 17 years old as the novel starts off and originally from Buckinghamshire, England, but then living in Paris with her mother after a tragedy in the family.<br><br>
+It's a story, based on my own early days in Europe, pre-Brexit, of a young love, that didn't quite work out, of despair and desperation, of a second chance of sorts... and of looming threats of plague, war and violence, that if left unchecked will lead to tragic consequences.<br><br>",
+
+
+
+"The story of a young man at a week long folk festival. Basically just a festival journal I reworked and edited I guess... tho I did fictionalise it a bit here and there.<br><br>",
+
+
+"Two young men fool and carouse around New York City, there's a raft trip to a ghost island in this one and plenty of memories of myself being young and dumb and stupid...",
+
+
+" correlated all of the various short stories I wrote over the years into the same document. <br><br>
+There's a prequel to Lucy in here and various tales of outcasts and loners like myself...<br><br>" 
+
+);
+
+foreach ($doc_title_array as $key => $value) { ?> 
+
+ <div class="blog-post" bis_skin_checked="1">
+    <!-- <img src="img/blog_post_1.png" alt=""> -->
+    <div class="text-content" bis_skin_checked="1">
+        <!-- <span><a href="#">Admin</a> / <a href="#">16 September 2018</a> / <a href="#">Branding</a></span> -->
+        <h2><?php echo $value; ?></h2>
+        <p>
+            <?php echo $doc_content_array[$key]; ?>
+        </p>
+        <div class="simple-btn" bis_skin_checked="1">
+            <a href="img/docs/<?php echo $doc_file_array[$key]; ?>" target="_blank"><i class="fa fa-download"></i> Download Here</a>
         </div>
     </div>
+</div>
+        <?php } ?>
 
-    
+      </div>
+ <br><br><br>
+  
 
 <p>
 <br>
@@ -123,7 +161,4 @@ If you liked (or disliked...!) a novel you're welcome to let me know your though
             </div>
         </div>
     </div>
-
-    
-    
  <?php include('footer.php');?>
